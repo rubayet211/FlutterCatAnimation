685a43ef36c12f8de0c551f0e92b01e19cc425b5
rubayet211 <rubayet211@gmail.com>
2023-08-13 02:52:36 +0600

    3rd commit

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -5,9 +5,24 @@ class Home extends StatefulWidget {
   HomeState createState() => HomeState();
 }
 
-class HomeState extends State<Home> {
-  Animation<double> catAnimation;
-  AnimationController catController;
+class HomeState extends State<Home> with TickerProviderStateMixin {
+  late Animation<double> catAnimation;
+  late AnimationController catController;
+
+  @override
+  initState() {
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+      CurvedAnimation(parent: catController!, curve: Curves.easeIn),
+    );
+
+    catController.forward();
+  }
 
   Widget build(context) {
     return Scaffold(
@@ -19,6 +34,15 @@ class HomeState extends State<Home> {
   }
 
   Widget buildAnimation() {
-    return Cat();
+    return AnimatedBuilder(
+      animation: catAnimation!,
+      builder: (context, child) {
+        return Container(
+          child: child,
+          margin: EdgeInsets.only(top: catAnimation.value),
+        );
+      },
+      child: Cat(),
+    );
   }
 }
